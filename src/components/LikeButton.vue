80ee363d8d02dffee58fcdc2ab8eb73726e55e5e
SergioCaMi <sergiocami84@gmail.com>
2025-08-20 18:40:53 +0200

    sequential colors in LikeButton

--- src/components/LikeButton.vue
+++ src/components/LikeButton.vue
@@ -12,11 +12,15 @@ import { ref } from "vue";
 const colores = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
 
 const counter = ref(0);
-const color = ref('gray'); 
-
+const color = ref('purple'); 
+const indexColor = ref(0)
 const btnColor = () => {
-  
-  return colores[Math.floor(Math.random() * colores.length)];
+  indexColor.value++
+  if (indexColor.value==colores.length){
+    indexColor.value=0
+  }
+  return colores[indexColor.value];
+  // return colores[Math.floor(Math.random() * colores.length)];
 };
 
 const countLikes = () => {
